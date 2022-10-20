74b86612166e859e5e4d10e3daabd31658050e8c
Marcelo918 <111820089+Marcelo918@users.noreply.github.com>
2022-10-20 15:13:09 -0700

    Completed tasks 1 and 2. The maven project is working correctly.

---
+++ nassef_egyption_pyramid_app/src/main/java/nassef_egyptian_pyramids_app/App.java
@@ -0,0 +1,14 @@
+package nassef_egyptian_pyramids_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Why is this yellow?" );
+        System.out.println("I'll print out another statement to test it.");
+    }
+}
